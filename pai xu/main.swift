44fe023adbaef8fe27cfbc1af3d59016645fdfb3
tshandan <496509048@qq.com>
2016-06-21 10:18:02 +0800

    pai xu
    使用冒泡排序给数组里的数排列顺序，有用到for in循环，条件判断语句。这最主要的还是超届问题，对于循环的次数要准确判断，不然就会出错。

--- pai xu/main.swift
+++ pai xu/main.swift
@@ -7,6 +7,21 @@
 //
 
 import Foundation
+var i = 0
+var j = 0
+var temp = 0
+var arr = [12,55,33,1,6]
+for j in 0...3{
+    for i in 0...3{
+        if(arr[i]>arr[i+1])
+        {
+        temp=arr[i]
+        arr[i]=arr[i+1]
+            arr[i+1]=temp
+        }
+    
+    }
 
-print("Hello, World!")
+}
+print(arr)
 
